42d986416e6f582f058fe314db5dcf9e1ba90d80
Demi van Kesteren <demi.van.kesteren@gmail.com>
2019-10-26 01:30:16 +0200

    First commit: application works

---
+++ bullsAndCows.hs
@@ -0,0 +1,58 @@
+import Data.List
+import System.Random
+
+main :: IO()
+main = bullsAndCows
+
+numberLength = 4 :: Int
+minRange = '1'
+maxRange = '9'
+
+bullsAndCows = do
+    numberToGuess <- getStdRandom(generateNumberToGuess numberLength [minRange .. maxRange])
+    putStrLn "Bulls and Cows: Game started!"
+    loop numberToGuess
+    where loop numberToGuess = do
+            input <- getLine
+            if isInputValid input
+                then 
+                    let (bulls, cows) = generateScore numberToGuess input in
+                    if bulls == numberLength
+                        then
+                            putStrLn ("You won the game! The answer was " ++ show numberToGuess)
+                    else do
+                            putStrLn (show bulls ++ " bulls, " ++ show cows ++ " cows")
+                            loop numberToGuess
+                else do
+                    putStrLn "Input is not valid"
+                    loop numberToGuess
+
+generateNumberToGuess :: Int -> [a] -> StdGen -> ([a], StdGen)
+generateNumberToGuess numberLength list generator = f numberLength list generator (length list - 1) []
+  where  f 0 _ generator _   ps = (ps, generator)
+         f numberLength list generator max ps =
+             f (numberLength - 1) (left ++ right) generator' (max - 1) (picked : ps)
+          where (index, generator') = randomR (0, max) generator
+                (left, picked : right) = splitAt index list
+
+isInputValid :: String -> Bool
+isInputValid input = 
+    isEveryNumberUnique input &&
+    length input == numberLength &&
+    all predicate input
+    where predicate p = (minRange <= p) && (p <= maxRange)
+
+isEveryNumberUnique :: String -> Bool
+isEveryNumberUnique input = do
+    let tempInput = input
+    let nubInput = nub input
+    if nubInput == tempInput
+        then 
+            True
+        else 
+            False
+
+generateScore :: String -> String -> (Int, Int)
+generateScore answer guess = (length bulls, cows)
+  where (bulls, others) = partition (uncurry (==)) (zip answer guess)
+        cows = length (uncurry intersect (unzip others))
